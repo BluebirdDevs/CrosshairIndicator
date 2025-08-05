61cdf88278f53e33fea966100ce2fc4719545d9a
Bluebird <76973207+BluebirdDevs@users.noreply.github.com>
2025-08-05 00:28:06 -0400

    update to 1.21.6(8)
    * update to 1.21.6?
    
    * bump fabric version
    
    * bump fabric loom version
    
    * Update gradle.properties
    
    * Update gradle-wrapper.properties
    
    * ;P
    
    * use renderpipeline
    
    * idk
    
    * I...
    
    * WORK
    
    * i giv eup
    
    * test
    
    * Update MixinInGameHud.java
    
    * test
    
    * Update MixinInGameHud.java
    
    * renderCrosshair removed so try isTranlucent
    
    * drawTexture instead
    
    * Update MixinInGameHud.java
    
    * there is no renderpipeline?
    
    * ts aint gonna work
    
    * Update MixinInGameHud.java
    
    * fsdagdsagdas
    
    * Update MixinInGameHud.java
    
    * Update MixinInGameHud.java
    
    * I DID IT WOOOO
    
    * only show if crosshair is shown
    
    * shouldShowCrosshair is for debug crosshair
    
    * use drawGuiTexture
    
    * Update MixinInGameHud.java
    
    * oops
    
    ---------

--- src/main/java/dog/crosshairindicator/mixin/MixinInGameHud.java
+++ src/main/java/dog/crosshairindicator/mixin/MixinInGameHud.java
@@ -1,39 +1,39 @@
 package dog.crosshairindicator.mixin;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.gl.RenderPipelines;
 import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Identifier;
-import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.gui.DrawContext;
 
 @Mixin(InGameHud.class)
 public class MixinInGameHud {
-	@Shadow @Final private MinecraftClient client;
-	@Unique Identifier CUSTOM_CROSSHAIR = Identifier.of("crosshairindicator", "crosshair");
-	@Unique Identifier SHIELD_CROSSHAIR = Identifier.of("crosshairindicator", "shield_crosshair")
+    @Shadow @Final private MinecraftClient client;
+
+    @Unique
+    private static final Identifier CUSTOM_CROSSHAIR = Identifier.of("crosshairindicator", "crosshair");
+
+    @Unique
+    private static final Identifier SHIELD_CROSSHAIR = Identifier.of("crosshairindicator", "shield_crosshair");
 
     @Inject(method = "renderCrosshair", at = @At("TAIL"))
-	private void drawCrosshair(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
-        if (this.client.targetedEntity instanceof PlayerEntity) {
-            int scaledWidth = 15;
-            int scaledHeight = 15;
+    private void drawCrosshair(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
+        if (this.client.targetedEntity instanceof PlayerEntity player && !this.client.inGameHud.shouldRenderCrosshair()) {
+            int size = 15;
+            int x = (context.getScaledWindowWidth() - size) / 2;
+            int y = (context.getScaledWindowHeight() - size) / 2;
 
-            if (this.client.targetedEntity.isBlocking()) {
-                context.drawGuiTexture(RenderLayer::getCrosshair, SHIELD_CROSSHAIR, (context.getScaledWindowWidth() - scaledWidth) / 2, (context.getScaledWindowHeight() - scaledHeight) / 2, scaledWidth, scaledHeight);
-            } else {
-                context.drawGuiTexture(RenderLayer::getCrosshair, CUSTOM_CROSSHAIR, (context.getScaledWindowWidth() - scaledWidth) / 2, (context.getScaledWindowHeight() - scaledHeight) / 2, scaledWidth, scaledHeight);
-            }
+            Identifier texture = player.isBlocking() ? SHIELD_CROSSHAIR : CUSTOM_CROSSHAIR;
+            context.drawGuiTexture(RenderPipelines.CROSSHAIR, texture, x, y, size, size);
         }
     }
-
 }
